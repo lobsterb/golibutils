6f2a7c6b570d7d893aa73395bdf7d918f7d415cd
lobsterb <57263573@qq.com>
2022-06-10 12:26:19 +0800

    添加string工具类-规范路径的函数, 将路径中所有\\替换成/

--- utils/string.go
+++ utils/string.go
@@ -6,3 +6,18 @@ import "strings"
 func ToLower(str string) string {
 	return strings.ToLower(str)
 }
+
+// NormativePath 规范路径
+func NormativePath(path string) string {
+	return strings.Replace(path, "\\", "/", -1)
+}
+
+// NormativeDirPath 规范目录路径
+func NormativeDirPath(path string) string {
+	dirPath := NormativePath(path)
+	// 判断最后一个字符是否是/
+	if dirPath[len(dirPath)-1:] != "/" {
+		dirPath = dirPath + "/"
+	}
+	return dirPath
+}
